00876f8ba411a988c1f72d92ec1d25460f89ba52
alenamandrika <Alenenok0@ukr.net>
2018-02-16 20:55:01 +0200

    initial commit

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,18 +1,48 @@
-const mongoose = require('mongoose')
+const express = require('express')
 
-const todoSchema = mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
+const Todo = require('./model')
+
+const router = express.Router()
+
+router.get('/todo', (req, res, next) => {
+  Todo.find({},)
+    .then(todo => {
+      res.json({todo})
+    })
+    .catch(next)
 })
 
-const Todo = mongoose.model('Todo', todoSchema)
+router.post('/todo', (req, res, next) => {
+  new Todo(req.body.todo)
+    .save()
+    .then(todo => {
+      console.log(todo)
+      res.json({todo})
+    })
+    .catch(next)
+})
 
-module.exports = { Todo }
+router.put('/todo/:id', function (req, res) {
+  Todo.findById(req.params.id, (err, todo) => {
+    todo.text = req.body.todo.text || todo.text
+    todo.url = req.body.todo.url || todo.url
+    todo.date = req.body.todo.date || todo.date
+    todo.save((err, todo) => {
+      if (err) {
+        res.status(400).json(err)
+      }
+      res.status(200).json(todo)
+    })
+  })
+})
+
+router.delete('/todo/:id', function (req, res) {
+  let id = req.params.id
+  Todo.remove({
+    _id: id
+  }, function () {
+    res.json()
+  })
+})
+
+module.exports = router

79fc4584f6f0ff841b67c61b5b078292c4b317d0
alenamandrika <Alenenok0@ukr.net>
2018-02-24 11:30:44 +0200

    added a ban on adding an empty

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -5,13 +5,12 @@ const Todo = require('../models/model')
 const router = express.Router()
 
 router.get('/todo/:id', (req, res) => {
-  Todo.findById({"_id": req.params.id})
-    .then(todo => {
-      res.json({todo})
-      if (!todo) {
-        return res.status(400).json(err)
-      }
-    })
+  Todo.findById({'_id': req.params.id}, (err, todo) => {
+    if (err) {
+      res.status(400).json(err)
+    }
+    res.status(200).json(todo)
+  })
 })
 
 router.get('/todo', (req, res, next) => {
@@ -22,23 +21,32 @@ router.get('/todo', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/todo', (req, res) => {
-  new Todo(req.body.todo)
-    .save()
-    .then(todo => {
-      res.json({todo})
-      if (!todo) {
-        return res.status(400).json(err)
-      }
-    })
+router.post('/todo', (req, res, next) => {
+ let newTodo = new Todo(req.body.todo)
+
+  if(req.body.todo.text !== undefined
+    && req.body.todo.complete !== undefined
+    && req.body.todo.date !== undefined
+    || req.body.todo.url!== undefined ) {
+    console.log('todo err , field is empty : ',req.body.todo);
+
+    res.json({success: false, message: 'Please enter text and url.'});
+  } else {
+    newTodo.save()
+      .then(todo => {
+        res.json({todo})
+      })
+      .catch(next)
+  }
+
 })
 
 router.put('/todo/:id', function (req, res) {
-  Todo.findOneAndUpdate({"_id": req.params.id},
+  Todo.findOneAndUpdate({'_id': req.params.id},
     {text: req.body.todo.text,
-     url: req.body.todo.url,
-     date: req.body.todo.date,
-     complete: req.body.todo.complete,
+      url: req.body.todo.url,
+      date: req.body.todo.date,
+      complete: req.body.todo.complete
     },
     {new: true},
     function (err, todo) {

904910705b0b27439f992477759d7996395a0940
alenamandrika <Alenenok0@ukr.net>
2018-03-08 00:15:03 +0200

    amended POST, PUT

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -23,50 +23,43 @@ router.get('/todo', (req, res, next) => {
 
 router.post('/todo', (req, res) => {
  let newTodo = new Todo(req.body.todo)
-
-  if (req.body.todo.text === ''
-    || req.body.todo.complete === ''
-    || req.body.todo.date === ''
-    || req.body.todo.url === '') {
-    console.log('todo err , field is empty : ',req.body.todo);
-
-    res.json({success: false, message: 'Please fill in the fields'});
-
-  } else {
+  if (req.body.todo.text !== ''
+    && req.body.todo.complete !== ''
+    && req.body.todo.date !== ''
+    && req.body.todo.url !== '') {
     newTodo.save()
       .then(todo => {
         res.json({todo})
       }, (err) => {
         res.status(400).json(err)
       })
+  } else {
+    console.log('todo err , field is empty : ',req.body.todo);
+    res.status(400).json({success: false, message: 'Please fill in the fields'});
   }
 })
 
 
 router.put('/todo/:id', function (req, res) {
   let todo = req.body.todo
-  for (let key in todo) {
-    if(todo.hasOwnProperty(key)) {
-      console.log((key + ':' + todo[key]))
-    } else {
-      console.log(key)
-    }
+  if (('text' in todo) && ('url' in todo) && ('date' in todo)) {
+    Todo.findOneAndUpdate({'_id': req.params.id},
+      {text: req.body.todo.text,
+        url: req.body.todo.url,
+        date: req.body.todo.date,
+        complete: req.body.todo.complete
+      },
+      {new: true},
+      function (err, todo) {
+        if (err) {
+          res.status(400).json(err)
+        }
+        res.status(200).json(todo)
+      })
+  } else {
+    res.status(400).json({success: false, message: 'unacceptable changes'});
   }
-  Todo.findOneAndUpdate({'_id': req.params.id},
-    {text: req.body.todo.text,
-      url: req.body.todo.url,
-      date: req.body.todo.date,
-      complete: req.body.todo.complete
-    },
-    {new: true},
-    function (err, todo) {
-      if (err) {
-        res.status(400).json(err)
-      }
-      res.status(200).json(todo)
-    })
 })
-
 
 router.delete('/todo/:id', function (req, res) {
   let id = req.params.id
